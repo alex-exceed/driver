noah-ellman/nodejs-driver

Use RequestExecution instances for each execution

lib/client.js
@@ -57,6 +57,13 @@ var warmupLimit = 32;
  * @property {RetryPolicy} policies.retry The retry policy.
  * @property {ReconnectionPolicy} policies.reconnection The reconnection policy to be used.
  * @property {AddressTranslator} policies.addressResolution The address resolution policy.
+ * @property {SpeculativeExecutionPolicy} policies.speculativeExecution The <code>SpeculativeExecutionPolicy</code>
+ * instance to be used to determine if the client should send speculative queries when the selected host takes more
+ * time than expected.
+ * <p>
+ *   Default: <code>[NoSpeculativeExecutionPolicy]{@link
+  *   module:policies/speculativeExecution~NoSpeculativeExecutionPolicy}</code>
+ * </p>
  * @property {TimestampGenerator} policies.timestampGeneration The client-side
  * [query timestamp generator]{@link module:policies/timestampGeneration~TimestampGenerator}.
  * <p>
@@ -385,7 +392,7 @@ Client.prototype._connectCb = function (callback) {
       if (!self.keyspace) {
         return next();
       }
-      self._setKeyspace(next);
+      RequestHandler.setKeyspace(self, next);
     },
     function setPoolOptionsAndWarmup(next) {
       //Set the pooling options depending on the protocol version
@@ -631,14 +638,14 @@ Client.prototype._batchCb = function (queries, options, callback) {
     return callback(options);
   }
 
-  var lbp = options.executionProfile.loadBalancing;
   utils.series([
     function connect(next) {
       self.connect(next);
     },
     function adaptQueries(next) {
       if (options.prepare) {
-        return PrepareHandler.getPreparedMultiple(self, lbp, queries, self.keyspace, next);
+        return PrepareHandler.getPreparedMultiple(
+          self, options.executionProfile.loadBalancing, queries, self.keyspace, next);
       }
       var parsedQueries = new Array(queries.length);
       for (var i = 0; i < queries.length; i++) {
@@ -659,8 +666,8 @@ Client.prototype._batchCb = function (queries, options, callback) {
       return callback(err);
     }
     var request = new requests.BatchRequest(queryItems, options);
-    var handler = new RequestHandler(self, lbp, options.retry);
-    handler.send(request, options, callback);
+    var handler = new RequestHandler(request, options, self);
+    handler.send(callback);
   });
 };
 
@@ -700,6 +707,7 @@ Client.prototype._shutdownCb = function (callback) {
     var hosts = self.hosts.values();
     // Shutdown the ControlConnection before shutting down the pools
     self.controlConnection.shutdown();
+    self.options.policies.speculativeExecution.close();
     // go through all the host and shut down their pools
     utils.each(hosts, function (h, next) {
       h.shutdown(false, next);
@@ -826,8 +834,8 @@ Client.prototype._innerExecute = function (query, params, options, callback) {
         query,
         params,
         options);
-      var handler = new RequestHandler(self, options.executionProfile.loadBalancing, options.retry);
-      handler.send(request, options, next);
+      var handler = new RequestHandler(request, options, self);
+      handler.send(next);
     }
   ], callback);
 };
@@ -869,20 +877,10 @@ Client.prototype._executeAsPrepared = function (query, params, options, callback
         params,
         options);
       request.query = query;
-      var handler = new RequestHandler(self, options.executionProfile.loadBalancing, options.retry);
-      handler.send(request, options, next);
+      var handler = new RequestHandler(request, options, self);
+      handler.send(next);
     }
   ], callback);
-};
-
-/**
- * Sets the keyspace in a connection that is already opened.
- * @param {Function} callback
- * @private
- */
-Client.prototype._setKeyspace = function (callback) {
-  var handler = new RequestHandler(this, this.options.policies.loadBalancing, this.options.policies.retry);
-  handler.setKeyspace(callback);
 };
 
 /**

alex-exceed/driver

lib/client.js
@@ -59,6 +59,13 @@ var warmupLimit = 32;
  * @property {RetryPolicy} policies.retry The retry policy.
  * @property {ReconnectionPolicy} policies.reconnection The reconnection policy to be used.
  * @property {AddressTranslator} policies.addressResolution The address resolution policy.
+ * @property {SpeculativeExecutionPolicy} policies.speculativeExecution The <code>SpeculativeExecutionPolicy</code>
+ * instance to be used to determine if the client should send speculative queries when the selected host takes more
+ * time than expected.
+ * <p>
+ *   Default: <code>[NoSpeculativeExecutionPolicy]{@link
+  *   module:policies/speculativeExecution~NoSpeculativeExecutionPolicy}</code>
+ * </p>
  * @property {TimestampGenerator} policies.timestampGeneration The client-side
  * [query timestamp generator]{@link module:policies/timestampGeneration~TimestampGenerator}.
  * <p>
@@ -387,7 +394,7 @@ Client.prototype._connectCb = function (callback) {
       if (!self.keyspace) {
         return next();
       }
-      self._setKeyspace(next);
+      RequestHandler.setKeyspace(self, next);
     },
     function setPoolOptionsAndWarmup(next) {
       //Set the pooling options depending on the protocol version
@@ -633,14 +640,14 @@ Client.prototype._batchCb = function (queries, options, callback) {
     return callback(options);
   }
 
-  var lbp = options.executionProfile.loadBalancing;
   utils.series([
     function connect(next) {
       self.connect(next);
     },
     function adaptQueries(next) {
       if (options.prepare) {
-        return PrepareHandler.getPreparedMultiple(self, lbp, queries, self.keyspace, next);
+        return PrepareHandler.getPreparedMultiple(
+          self, options.executionProfile.loadBalancing, queries, self.keyspace, next);
       }
       var parsedQueries = new Array(queries.length);
       for (var i = 0; i < queries.length; i++) {
@@ -661,8 +668,8 @@ Client.prototype._batchCb = function (queries, options, callback) {
       return callback(err);
     }
     var request = new requests.BatchRequest(queryItems, options);
-    var handler = new RequestHandler(self, lbp, options.retry);
-    handler.send(request, options, callback);
+    var handler = new RequestHandler(request, options, self);
+    handler.send(callback);
   });
 };
 
@@ -714,6 +721,7 @@ Client.prototype._shutdownCb = function (callback) {
     var hosts = self.hosts.values();
     // Shutdown the ControlConnection before shutting down the pools
     self.controlConnection.shutdown();
+    self.options.policies.speculativeExecution.shutdown();
     // go through all the host and shut down their pools
     utils.each(hosts, function (h, next) {
       h.shutdown(false, next);
@@ -840,8 +848,8 @@ Client.prototype._innerExecute = function (query, params, options, callback) {
         query,
         params,
         options);
-      var handler = new RequestHandler(self, options.executionProfile.loadBalancing, options.retry);
-      handler.send(request, options, next);
+      var handler = new RequestHandler(request, options, self);
+      handler.send(next);
     }
   ], callback);
 };
@@ -883,20 +891,10 @@ Client.prototype._executeAsPrepared = function (query, params, options, callback
         params,
         options);
       request.query = query;
-      var handler = new RequestHandler(self, options.executionProfile.loadBalancing, options.retry);
-      handler.send(request, options, next);
+      var handler = new RequestHandler(request, options, self);
+      handler.send(next);
     }
   ], callback);
-};
-
-/**
- * Sets the keyspace in a connection that is already opened.
- * @param {Function} callback
- * @private
- */
-Client.prototype._setKeyspace = function (callback) {
-  var handler = new RequestHandler(this, this.options.policies.loadBalancing, this.options.policies.retry);
-  handler.setKeyspace(callback);
 };
 
 /**